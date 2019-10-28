1f119ae3ee5c5dc9725786ba533d86325b5061db
boba1987 <boba_dj1987@hotmail.com>
2019-10-28 14:28:11 +0100

    initial setup

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { pageLinks } from '../../routes/config';
+import { navigationLinks } from '../../routes/config';
 import { NavLink } from 'react-router-dom';
 
 function Navigation() {
 	return (
 		<div>
-			<a href="/">Logo</a>
+			<NavLink className="navigation-logo" to={"/"}>Logo</NavLink>
 
 			<ul>
-				{ pageLinks.map(({label, to, exact}, index) =>
+				{ navigationLinks.map(({label, to, exact}, index) =>
 					<li key={index}>
 						<NavLink to={to} exact={exact} activeClassName="selected">{label}</NavLink>
 					</li>) }
--- src/routes/config/index.js
+++ src/routes/config/index.js
@@ -1,29 +1,27 @@
 import routes from '../../constants/routes';
 import RouteGuard from '../../components/routeGuard';
-import Home from '../Home';
 import UserProfile from '../UserProfile';
 import NotFound from '../NotFound';
 import Login from '../Login';
+import Home from '../Home';
 
-export const pageLinks = [
+export const navigationLinks = [
     {
         label: 'home',
         to: routes.home,
         exact: true,
         path: routes.home,
         component: Home,
-    },
+    }
+]
+
+export default [
+    ...navigationLinks,
     {
-        label: 'profile',
-        to: routes.profile,
         path: routes.profile,
         tag: RouteGuard,
         component: UserProfile
     },
-]
-
-export default [
-    ...pageLinks,
     {
         path: routes.login,
         component: Login
